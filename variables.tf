05812f0db41fd05ea88962fe1381d6efca4871bc
Julicopet <144615871+Julicopet@users.noreply.github.com>
2023-10-20 10:32:36 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-2" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
@@ -12,6 +12,12 @@ variable "instance_type" {
     description = "instance type for ec2" 
     default     =  "t2.micro" 
 }
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "jenkins-sgroup-dec-2021" 
+}
+
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
     default     = "my-ec2-instance" 
